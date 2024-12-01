0ec8fbd073f86612d486ab980b2666167f751474
maksi <maksimozarinskij@gmail.com>
2024-12-01 18:15:08 +0200

    01.12.2024

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-// Функція для відображення відповідного контенту
 function showContent(pageId) {
-    // Сховати всі контент-блоки
     var contentSections = document.querySelectorAll('.content');
     contentSections.forEach(function(section) {
         section.classList.remove('active');
+        section.style.animation = 'none';
     });
 
-    // Показати вибраний контент
     var activePage = document.getElementById(pageId);
     activePage.classList.add('active');
+
+    activePage.style.animation = 'fadeIn 1s ease-out forwards';
+
+    localStorage.setItem('activePage', pageId);
 }
+
+window.onload = function() {
+    var activePage = localStorage.getItem('activePage');
+
+    if (activePage) {
+        showContent(activePage);
+    } else {
+        showContent('home');
+    }
+};
